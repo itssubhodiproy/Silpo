46e0d87b0bc516703a6b23c485568dc1696aa316
subh-23 <subhodiproy281@gmail.com>
2022-10-18 20:22:53 +0530

    better cart with remove func

--- routes/customer.js
+++ routes/customer.js
@@ -3,7 +3,7 @@ const route = express.Router();
 const { authRole, checkAuthenticated } = require("../roleAuth");
 const ROLE = require("../roles");
 const Product = require("../model/products");
-const Order = require("../model/orders");
+var Order = require("../model/orders");
 var Users = require("../model/users");
 
 //giving customer all the listed product
@@ -18,7 +18,7 @@ route.get(
       res.render("customer/customer.ejs", { products: products });
     } catch (error) {
       throw error;
-      res.error(500).send("Server Error");
+      // res.error(500).send("Server Error");
     }
   }
 );
@@ -66,58 +66,89 @@ route.get(
   }
 );
 
-route.post("/cart", async (req, res) => {
-  try {
-    const order = await Order.create({
-      customer_name: req.user.name,
-      customer_address: req.body.address,
-      contact_number: req.body.number,
-      order_time: Date.now().toLocaleString(),
-      // allProducts: cartProducts,
-      totalPrice: totalPrice,
-      customer_id: customer_id,
-      allProducts: req.user.cart,
-    });
-    await Users.findByIdAndUpdate(req.user.id, {
-      $set: { cart: [] },
-    });
-    res.redirect("/orders");
-  } catch (error) {
-    res.send("you're facing error");
-  }
-});
-
-//customer is placing order
 route.post(
-  "/order/:id",
+  "/cart",
   checkAuthenticated,
   authRole(ROLE.CUSTOMER),
   async (req, res) => {
-    const cartId = req.params.id;
-    const customer_id = req.user._id;
     try {
-      const cartProducts = await Cart.findById(cartId);
-      //sum of all the products
+      // sum of all the products
       var totalPrice = 0;
-      cartProducts.array.forEach((element) => {
-        totalPrice += element.price;
+      var cartProducts = req.user.cart;
+      console.log(cartProducts);
+      cartProducts.forEach((element) => {
+        totalPrice += parseInt(element.price, 10);
       });
-      //creating order
+      console.log(totalPrice);
+      //creating a new order
       await Order.create({
         customer_name: req.user.name,
-        customer_address: req.body.address,
-        contact_number: req.body.number,
-        order_time: Date.now().toLocaleString(),
-        allProducts: cartProducts,
+        customer_address: "req.body.address",
+        contact_number: "req.body.number",
+        order_time: Date.now(),
+        // allProducts: cartProducts,
         totalPrice: totalPrice,
-        customer_id: customer_id,
+        customer_id: req.user.id,
+        allProducts: cartProducts,
+      });
+      await Users.findByIdAndUpdate(req.user.id, {
+        $set: { cart: [] },
       });
       res.redirect("/customer/status");
     } catch (error) {
-      res.status(400).send("You're facing error");
+      res.send("you're facing error");
     }
   }
 );
+
+//deleting the product from cart
+route.get(
+  "/cart/:id",
+  checkAuthenticated,
+  authRole(ROLE.CUSTOMER),
+  async (req, res) => {
+    try {
+      await Users.findByIdAndUpdate(req.user.id, {
+        $pull: { cart: { _id: req.params.id } },
+      });
+      res.redirect("/customer/cart");
+    } catch (error) {
+      res.send("you're facing error");
+    }
+  }
+);
+
+//customer is placing order
+// route.post(
+//   "/order/:id",
+//   checkAuthenticated,
+//   authRole(ROLE.CUSTOMER),
+//   async (req, res) => {
+//     const cartId = req.params.id;
+//     const customer_id = req.user._id;
+//     try {
+//       const cartProducts = await Cart.findById(cartId);
+//       //sum of all the products
+//       var totalPrice = 0;
+//       cartProducts.array.forEach((element) => {
+//         totalPrice += element.price;
+//       });
+//       //creating order
+//       await Order.create({
+//         customer_name: req.user.name,
+//         customer_address: req.body.address,
+//         contact_number: req.body.number,
+//         order_time: Date.now().toLocaleString(),
+//         allProducts: cartProducts,
+//         totalPrice: totalPrice,
+//         customer_id: customer_id,
+//       });
+//       res.redirect("/customer/status");
+//     } catch (error) {
+//       res.status(400).send("You're facing error");
+//     }
+//   }
+// );
 //your all current and past orders
 route.get(
   "/status",
@@ -127,7 +158,8 @@ route.get(
     const id = req.user._id;
     try {
       const allOrders = await Order.find({ customer_id: id });
-      res.render("customer/customerStatus.ejs", { orders: allOrders });
+      // res.render("customer/customerStatus.ejs", { orders: allOrders });
+      res.json(allOrders);
     } catch (error) {
       res.send("you're facing error");
     }
